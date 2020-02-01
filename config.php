8bccaf42ac997aa4d9f6e49cbc30b5f2e79e8548
jean lefebvre <jean.lefebvre1181@gmail.com>
2020-02-02 00:57:29 +0100

    init config + init model.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Site url
+$url = "http://localhost/P4/";
+
+// Database
+$db = [
+    'host'=> 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'projet4'
+];
